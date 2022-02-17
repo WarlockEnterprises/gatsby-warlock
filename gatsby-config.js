aab7dcafb13f2b234f9bc3cdde33ab948adf6f51
Tim Sandberg <tasandberg@gmail.com>
2022-02-17 10:41:24 -0800

    Get started on header

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Warlock Enterprises`,
+    description: `Official shop for Warlock Enterprises prints, apparel, and NFTs.`,
+    author: `tasandberg@gmail.com`,
+    siteUrl: `https://warlockenterprises.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,60 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import Navbar from "react-bootstrap/Navbar"
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import { StaticImage } from "gatsby-plugin-image"
+
+const links = [
+  {
+    label: "Apparel",
+    color: "warlock-red",
+    path: "/apparel",
+  },
+  {
+    label: "Prints",
+    color: "warlock-blue",
+    path: "/prints",
+  },
+  {
+    label: "NFTs",
+    color: "warlock-orange",
+    path: "/nfts",
+  },
+  {
+    label: "Contact",
+    color: "warlock-green",
+    path: "/contact",
+  },
+]
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Navbar bg="white" expand="md">
+    <Container>
+      <Navbar.Brand href="#home">
+        <StaticImage src="../images/brand-logo.svg" alt="Warlock Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          {links.map(({ label, color, path }) => (
+            <Nav.Link
+              key={`header-link-${label}`}
+              as={Link}
+              to={path}
+              className="label me-5"
+            >
+              {label}
+            </Nav.Link>
+          ))}
+          <Nav.Link as={Link} to="/cart">
+            <ion-icon name="heart" />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 )
 
 Header.propTypes = {
